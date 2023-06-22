b37df61a32b87a9f659346bc7ad0ad25016e7e57
kani537 <94041681+kani537@users.noreply.github.com>
2023-06-22 23:58:19 +0900

    edit

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -19,10 +21,25 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+    [";"] = { ":", desc = "colon" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  i = {
+    ["<C-CR>"] = { "<Esc>o", desc = "Next line down" },
+    ["<S-CR>"] = { "<Esc>O", desc = "Next line up" },
+
+    -- go to  beginning and end
+    ["<C-b>"] = { "<ESC>^i", desc = "Beginning of line" },
+    ["<C-e>"] = { "<End>", desc = "End of line" },
+
+    -- navigate within insert mode
+    ["<C-h>"] = { "<Left>", desc = "Move left" },
+    ["<C-l>"] = { "<Right>", desc = "Move right" },
+    ["<C-j>"] = { "<Down>", desc = "Move down" },
+    ["<C-k>"] = { "<Up>", desc = "Move up" },
   },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,11 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "plugins.configs.luasnip" (plugin, opts)                                       -- include the default astronvim config that calls the setup call
+      require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } } -- load snippets paths
+    end,
+  },
 }
